98fbf49b6f93b8a1af06f8eef50a4342fc1b0a21
Tetsuro Nakamura <t2low.nk@gmail.com>
2019-07-29 01:51:56 +0900

    Intentでidとdestinationを渡す

--- app/src/main/java/com/tappli/bustimetable/presentation/BusScheduleActivity.kt
+++ app/src/main/java/com/tappli/bustimetable/presentation/BusScheduleActivity.kt
@@ -12,12 +12,23 @@ class BusScheduleActivity : AppCompatActivity() {
 
     companion object {
         fun createIntent(context: Context, busStop: BusStop, destination: Destination): Intent {
-            return Intent(context, BusScheduleActivity::class.java)
+            return Intent(context, BusScheduleActivity::class.java).apply {
+                putExtra(Parameter.BusStopId.name, busStop.id.value)
+                putExtra(Parameter.Destination.name, destination)
+            }
         }
+    }
+
+    private enum class Parameter {
+        BusStopId,
+        Destination
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bus_schedule)
+
+        val busStopId = intent.getIntExtra(Parameter.BusStopId.name, 0)
+        val destination = intent.getSerializableExtra(Parameter.Destination.name) as Destination
     }
 }
