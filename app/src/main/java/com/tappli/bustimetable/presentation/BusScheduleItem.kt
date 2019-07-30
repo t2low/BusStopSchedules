09e1af60ca84c8db71f9aef577f11303519f8cb0
Tetsuro Nakamura <t2low.nk@gmail.com>
2019-07-31 01:43:04 +0900

    時刻(分)をタップしたときに画面遷移させたかったので、テキストではなくボタンを並べたUIに変更する

--- app/src/main/java/com/tappli/bustimetable/presentation/BusScheduleItem.kt
+++ app/src/main/java/com/tappli/bustimetable/presentation/BusScheduleItem.kt
@@ -1,10 +1,13 @@
 package com.tappli.bustimetable.presentation
 
+import android.view.LayoutInflater
+import android.view.ViewGroup
 import com.tappli.bustimetable.R
 import com.tappli.bustimetable.domain.BusSchedule
 import com.xwray.groupie.kotlinandroidextensions.Item
 import com.xwray.groupie.kotlinandroidextensions.ViewHolder
 import kotlinx.android.synthetic.main.view_bus_schedule.view.*
+import kotlinx.android.synthetic.main.view_minutes_item.view.*
 
 data class BusScheduleItem(private val schedules: List<BusSchedule>) : Item() {
 
@@ -12,9 +15,19 @@ data class BusScheduleItem(private val schedules: List<BusSchedule>) : Item() {
 
     override fun bind(viewHolder: ViewHolder, position: Int) {
         val hour = schedules.first().hours
-        val minutes = schedules.map { it.minutes }
-        viewHolder.itemView.scheduleHourTextView.text = String.format("%2d", hour)
-        viewHolder.itemView.scheduleMinutesTextView.text =
-            minutes.joinToString("  ") { String.format("%02d", it) }
+        viewHolder.itemView.scheduleHourTextView.text = String.format("%02d", hour)
+        val inflater = LayoutInflater.from(viewHolder.itemView.context)
+        schedules
+            .map {
+                inflater.inflate(R.layout.view_minutes_item, null).apply {
+                    minutesButton.text = String.format("%02d", it.minutes)
+                    minutesButton.setOnClickListener {
+                        // TODO:
+                    }
+                }
+            }
+            .forEach {
+                viewHolder.itemView.minutesItemContainer.addView(it)
+            }
     }
 }
